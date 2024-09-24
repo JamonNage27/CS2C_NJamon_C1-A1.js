b5ad24fdc0554e5bc58aaeebdd9d3551dee71912
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-09-25 00:14:56 +0800

    Update CS2C_NJamon_C1-A1.js

--- CS2C_NJamon_C1-A1.js
+++ CS2C_NJamon_C1-A1.js
@@ -7,29 +7,29 @@ let dream = "Computer Programmer";
 let caps = given.toUpperCase();
 let lows = given.toLowerCase();
 
-let in = caps + "was born " + birthday + "at " + birthplace + ", and currently living at " + address + ", " + lows + "is taking up " + course + "and dreams to be " + dream + "after graduation";
-console.log(in);
+let infos = caps + "was born " + birthday + "at " + birthplace + ", and currently living at " + address + ", " + lows + "is taking up " + course + "and dreams to be " + dream + "after graduation";
+console.log(infos);
 
 let givenName = "Mikael Jas Melo ";
 let birthdate = "September 27, 2005 ";
 let bplace = "City of Candon Hospital, Candon City,Philippines 2710";
-let address = "Gabur Sur, Sta.Cruz ,Philippines 2713 ";
-let course = "Bachelor of Science and Computer Science ";
-let dream = "Game Developer";
-let high = given.toUpperCase();
-let low = given.toLowerCase();
+let address2 = "Gabur Sur, Sta.Cruz ,Philippines 2713 ";
+let course2 = "Bachelor of Science and Computer Science ";
+let dream2 = "Game Developer";
+let caps2 = given.toUpperCase();
+let lows2 = given.toLowerCase();
 
-let in = high + "was born " + birthdate + "at " + bplace + ", and currently living at " + address + ", " + lows + "is taking up " + course + "and dreams to be " + dream + "after graduation";
+let in = caps2 + "was born " + birthdate + "at " + bplace + ", and currently living at " + address2 + ", " + lows2 + "is taking up " + course2 + "and dreams to be " + dream2 + "after graduation";
 console.log(in);
 
-let name = "Jm Levine Mendoza ";
-let birthday = "July 07, 2005 ";
-let birthplace = "City of Candon Hospital, Candon City,Philippines 2710";
-let address = "Dili,Sta. Cruz, Ilocos Sur,Philippines 2713 ";
-let course = "Bachelor of Science and Computer Science ";
-let dream = "Computer Programmer";
-let caps = given.toUpperCase();
-let lows = given.toLowerCase();
+let name3 = "Jm Levine Mendoza ";
+let birthdate3= "July 07, 2005 ";
+let birthplace3 = "City of Candon Hospital, Candon City,Philippines 2710";
+let address3 = "Dili,Sta. Cruz, Ilocos Sur,Philippines 2713 ";
+let course3 = "Bachelor of Science and Computer Science ";
+let dream3 = "Computer Programmer";
+let caps3 = given.toUpperCase();
+let lows3 = given.toLowerCase();
 
-let in= caps + "was born " + birthdate + "at " + birthplace + ", and currently living at " + address + ", " + lows + "is taking up " + course + "and dreams to be " + dream + "after graduation";
-console.log(in);
+let info= caps3 + "was born " + birthdate3 + "at " + birthplace3 + ", and currently living at " + address3 + ", " + lows3 + "is taking up " + course3 + "and dreams to be " + dream3 + "after graduation";
+console.log(info);
