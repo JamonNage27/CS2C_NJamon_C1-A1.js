1ef5c022851fe2ef6188061782d4790a41175691
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-09-24 23:58:49 +0800

    Update CS2C_NJamon_C1-A1.js

--- CS2C_NJamon_C1-A1.js
+++ CS2C_NJamon_C1-A1.js
@@ -1,11 +1,35 @@
-let name = "Felix Garcia Gabriel ";
-let birthday = "April 11, 2005 ";
+let name = "Sophia Ky Mori ";
+let birthday = "December 22, 2005 ";
 let birthplace = "City of Candon Hospital, Candon City,Philippines 2710";
-let address = "Tablac,Candon City,Philippines 2710 ";
-let course = "bachelor of science and computer science ";
+let address = "San Nicolas,Candon City,Philippines 2710 ";
+let course = "Bachelor of Science and Computer Science ";
 let dream = "Computer Programmer";
 let caps = given.toUpperCase();
 let lows = given.toLowerCase();
 
-let mail = high + "was born " + birthdate + "at " + birthplace + ", and currently living at " + address + ", " + lows + "is taking up " + course + "and dreams to be " + dream + "after graduation";
-console.log(mail);
+let in = caps + "was born " + birthday + "at " + birthplace + ", and currently living at " + address + ", " + lows + "is taking up " + course + "and dreams to be " + dream + "after graduation";
+console.log(in);
+
+let givenName = "Mikael Jas Melo ";
+let birthdate = "September 27, 2005 ";
+let bplace = "City of Candon Hospital, Candon City,Philippines 2710";
+let address = "Gabur Sur, Sta.Cruz ,Philippines 2713 ";
+let course = "Bachelor of Science and Computer Science ";
+let dream = "Game Developer";
+let high = given.toUpperCase();
+let low = given.toLowerCase();
+
+let in = high + "was born " + birthdate + "at " + bplace + ", and currently living at " + address + ", " + lows + "is taking up " + course + "and dreams to be " + dream + "after graduation";
+console.log(in);
+
+let name = "Jm Levine Mendoza ";
+let birthday = "July 07, 2005 ";
+let birthplace = "City of Candon Hospital, Candon City,Philippines 2710";
+let address = "Dili,Sta. Cruz, Ilocos Sur,Philippines 2713 ";
+let course = "Bachelor of Science and Computer Science ";
+let dream = "Computer Programmer";
+let caps = given.toUpperCase();
+let lows = given.toLowerCase();
+
+let in= caps + "was born " + birthdate + "at " + birthplace + ", and currently living at " + address + ", " + lows + "is taking up " + course + "and dreams to be " + dream + "after graduation";
+console.log(in);
