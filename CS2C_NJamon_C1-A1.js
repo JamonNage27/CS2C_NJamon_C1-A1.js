6c29fd10cf210e47e2757350f54a3948238bc3b9
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-09-24 23:25:06 +0800

    Create CS2C_NJamon_C1-A1.js

---
+++ CS2C_NJamon_C1-A1.js
@@ -0,0 +1,11 @@
+let name = "Felix Garcia Gabriel ";
+let birthday = "April 11, 2005 ";
+let birthplace = "City of Candon Hospital, Candon City,Philippines 2710";
+let address = "Tablac,Candon City,Philippines 2710 ";
+let course = "bachelor of science and computer science ";
+let dream = "Computer Programmer";
+let caps = given.toUpperCase();
+let lows = given.toLowerCase();
+
+let mail = high + "was born " + birthdate + "at " + birthplace + ", and currently living at " + address + ", " + lows + "is taking up " + course + "and dreams to be " + dream + "after graduation";
+console.log(mail);
