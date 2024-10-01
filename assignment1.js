cc4eec8dd1c440358b0193e86889480007800fe6
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-10-02 07:12:03 +0800

    assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,15 @@
+let name = "Sophia Ky Mori ";
+let birthday = "December 22, 2005 ";
+let birthplace = "City of Candon Hospital, Candon City,Philippines 2710";
+let address = "San Nicolas,Candon City,Philippines 2710 ";
+let course = "Bachelor of Science and Computer Science ";
+let dream = "Computer Programmer";
+let caps = given.toUpperCase();
+let lows = given.toLowerCase();
+let addressupper =address.toLocaleUpperCase();
+let bplaceupper = bplace.toLocaleUpperCase();
+
+let info = caps + "was born " + birthday + "at " + birthplace + ", and currently living at " + address + ", " + lows + "is taking up " + course + "and dreams to be " + dream + "after graduation";
+console.log(info);
+
+
